4ba7c6dbf9b109f05a30a5dc16d7f72012626760
Jc Ayala <j.ayala.higuera@gmail.com>
2021-02-08 18:32:00 -0300

    solucion bug ruta

--- src/app/shared/components/nav-courses/nav-courses.component.ts
+++ src/app/shared/components/nav-courses/nav-courses.component.ts
@@ -7,13 +7,25 @@ import { Link } from '../../../core/models/link.model';
   styleUrls: ['./nav-courses.component.scss'],
 })
 export class NavCoursesComponent implements OnInit {
+  /**
+   * @description arreglo que contiene la estructura de capitulos del curso
+   */
   chapters: Link[];
+  /**
+   * @description arreglo que contiene la estructura de capitulos del curso
+   */
+  /**
+   * @description variable que controla si se ve o no el toggle del header
+   */
   menuVisible = false;
 
   constructor(private utilsService: UtilsService) {}
 
   ngOnInit(): void {}
 
+  /**
+   * @description metodo que entrage en el estado actual del toggle-menu en el header
+   */
   activeSidebar(event) {
     this.menuVisible = !this.menuVisible;
     this.utilsService.sendStatusSidebar(this.menuVisible);
