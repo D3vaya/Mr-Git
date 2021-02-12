395b7f3b5f049214e7acd608f9658ac605552bbb
Jc Ayala <j.ayala.higuera@gmail.com>
2021-02-12 00:24:08 -0300

    se cambio el routing de la pagina page not found

--- src/app/shared/components/page-not-found/page-not-found.component.ts
+++ src/app/shared/components/page-not-found/page-not-found.component.ts
@@ -1,20 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.scss']
+  styleUrls: ['./page-not-found.component.scss'],
 })
 export class PageNotFoundComponent implements OnInit {
+  constructor() {}
 
-  constructor(private location: Location) { }
-  
-  goBack() {
-    this.location.back();
-  }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
