692bbc789513deb00cfe00f435352146bb7c2112
Jc Ayala <j.ayala.higuera@gmail.com>
2021-02-13 03:10:01 -0300

    eliminacion de archivos

--- src/environments/firebase.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyAjfK9qlwXrJuZ0osruLvVss5f43cbcdkI',
-  authDomain: 'mrgit-9e7db.firebaseapp.com',
-  projectId: 'mrgit-9e7db',
-  storageBucket: 'mrgit-9e7db.appspot.com',
-  messagingSenderId: '470361493338',
-  appId: '1:470361493338:web:9926289f541d87e272ed63',
-  measurementId: 'G-D3QPYRQ8VF',
-};
